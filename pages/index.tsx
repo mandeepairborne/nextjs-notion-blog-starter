7a884157ba31b8bada1792138c73b215d326cd95
Mandeep <mandeepjangir@protonmail.com>
2024-06-25 11:07:09 +0000

    h

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { convertToArticleList, getAllArticles, notion } from 'utils/notion';
 import { Layout } from 'layouts/Layout';
-import HeroHeader from 'components/HeroHeader';
+
 import Container from 'components/Container';
 import { Fragment, useState } from 'react';
 import { filterArticles } from 'utils/filterArticles';
@@ -8,28 +8,19 @@ import Category from 'components/Category';
 import ArticleCard from 'components/ArticleCard';
 
 export default function Index(props) {
-  const { articles, categories } = props;
+  const { articles, categories   } = props;
 
   const [selectedTag, setSelectedTag] = useState<string>(null);
   const filteredArticles = filterArticles(articles, selectedTag);
 
   return (
     <Layout>
-      <HeroHeader />
-      <div className="flex flex-wrap justify-center gap-4 mt-8">
-        {categories.map(tag => (
-          <Category
-            tag={tag}
-            key={tag}
-            selectedTag={selectedTag}
-            setSelectedTag={setSelectedTag}
-          />
-        ))}
-      </div>
+      
+  
       <Container>
         <div className="py-8">
           <div className="my-8 text-3xl font-bold text-gray-900">
-            {!selectedTag ? 'Latest articles' : `${selectedTag} articles`}
+         
           </div>
           <div className="grid gap-10 lg:gap-12 sm:grid-cols-2">
             {filteredArticles.map(article => (
